39b71837663f10384adff1714614b8efa61e6de8
Matt Stamm <102386284+stmmy@users.noreply.github.com>
2022-06-01 23:43:27 -0500

    Update transform.py
    much more efficient way of reading the data into the data frame, minimal transformation needed

--- transform.py
+++ transform.py
@@ -14,48 +14,11 @@ f = open(file_name)
 data = json.load(f)
 f.close()
 
-#Sort data
+#Create dataframe from json file
 fdata = []
 for entry in data["fact"]:
-   fdata.append([entry["dim"]["COUNTRY"], entry["dim"]["YEAR"], entry["dim"]["SEX"], float(entry["Value"][0:entry["Value"].find("[")-1])])
-sdata = sorted(fdata)
-
-
-#Get lists for pandas dataframe
-mapping = {"United States of America": "USA", "Canada": "CAN", "Japan": "JPN", "China": "CHN"} #THIS CODE NEEDS TO BE CHANGED FOR DIFF FILTERS
-country = []
-country_code = []
-year = []
-value_both = []
-value_female = []
-value_male = []
-
-num_of_years = 0
-for i in range(len(sdata)):
-    if sdata[i][1] not in year:
-        year.append(sdata[i][1])
-        num_of_years += 1
-
-num_of_countries = 0
-for i in range(len(sdata)):
-    if sdata[i][0] not in country:
-        for j in range(num_of_years):
-            country.append(sdata[i][0])
-            country_code.append(mapping[sdata[i][0]])
-        num_of_countries += 1
-    if sdata[i][2] == "Both sexes":
-        value_both.append(float(sdata[i][3]))
-    elif sdata[i][2] == "Female":
-        value_female.append(float(sdata[i][3]))
-    elif sdata[i][2] == "Male":
-        value_male.append(float(sdata[i][3]))
-year = year*num_of_countries 
-
-#Create Dataframe
-df = pd.DataFrame()
-df["Country"] = country
-df["CC"] = country_code
-df["Year"] = year
-df["Both Sexes"] = value_both
-df["Female"] = value_female
-df["Male"] = value_male
+    country_code = "USA" if entry["dim"]["COUNTRY"] == "United States of America" else entry["dim"]["COUNTRY"][:2].upper()
+    fdata.append([entry["dim"]["COUNTRY"],country_code,entry["dim"]["YEAR"],entry["dim"]["SEX"], float(entry["Value"][0:entry["Value"].find("[")-1])])
+df = pd.DataFrame(fdata)
+df.columns=["Contry", "Country_Code", "Year", "Sex","Value"]
+df.to_csv("C:/API&Pipeline/processed/{}_cig_metrics.csv".format(datetime.now().strftime("%Y%m%d%H%M%S")))
