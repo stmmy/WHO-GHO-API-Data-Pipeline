4948f24898355b4d8a1b64020cbe6db8374ac312
Matt Stamm <102386284+stmmy@users.noreply.github.com>
2022-06-13 15:33:07 -0500

    Update process.py

--- process.py
+++ process.py
@@ -1,23 +1,31 @@
 import pandas as pd
 import pyodbc
-import glob
+from datetime import datetime
 import os
 import shutil
 
-import sys
-
 def process(file_name):
-    #Read csv data into a dataframe
-    file_path = r"C:\API&Pipeline\process\{}".format(file_name)
-    df = pd.read_csv(file_path, index_col=0)
+    #Create log dataframe
+    logs = pd.DataFrame(columns = ["File_Name", "Log_Message", "Time_Stamp", "Stage_Of_File"])
 
-    #Connection to sql server
-    conn = pyodbc.connect('Driver={SQL Server};'
-                        'Server=BIGBERTHA;'
-                        'Database=stayfit_data_metrics;'
-                        'Trusted_Connection=yes;')
-    cursor = conn.cursor()
-    error = False
+
+    #Read csv data into a dataframe
+    try:
+        file_path = r"C:\API&Pipeline\process\{}".format(file_name)
+        df = pd.read_csv(file_path, index_col=0)
+
+        #Connection to sql server
+        conn = pyodbc.connect('Driver={SQL Server};'
+                            'Server=BIGBERTHA;'
+                            'Database=stayfit_data_metrics;'
+                            'Trusted_Connection=yes;')
+        cursor = conn.cursor()
+    except:
+        logs.loc[len(logs.index)] = [file_name, "Error in reading data and connecting to SQL server", datetime.now(), "Processing"]
+        logs.to_csv('logs.csv', mode='a', index=False, header=False)
+    else:
+        logs.loc[len(logs.index)] = [file_name, "Data read, connected to SQL server", datetime.now(), "Processing"]
+
 
     #Insert pd Dataframe int the created table
     try:
@@ -26,7 +34,7 @@ def process(file_name):
                         INSERT INTO cigarette_metrics (country_name, country_code, year, sex, value)
                         VALUES (?,?,?,?,?)
                         ''',
-                        row.Country, 
+                        row.Country,
                         row.Country_Code,
                         row.Year,
                         row.Sex,
@@ -34,16 +42,18 @@ def process(file_name):
                         )
         conn.commit()
     except Exception as exception_message:
-        print(exception_message, "\nData moved to errors.")
-        error = True
+        logs.loc[len(logs.index)] = [file_name, "Error in transferring finished data to SQL server - moved to local errors folder", datetime.now(), "Processing"]
+        logs.to_csv('logs.csv', mode='a', index=False, header=False)
         src_path = r"C:\API&Pipeline\process\{}".format(file_name)
         target_path = r"C:\API&Pipeline\errors\{}".format(file_name)
         shutil.copyfile(src_path, target_path)
         os.remove(src_path)
-
-    if error != True:
-        print("Data transferred successfully. Data archived.")
+        return False
+    else:
+        logs.loc[len(logs.index)] = [file_name, "Data transferred to SQL server successfully - data archived", datetime.now(), "Processing"]
+        logs.to_csv('logs.csv', mode='a', index=False, header=False)
         src_path = r"C:\API&Pipeline\process\{}".format(file_name)
         target_path = r"C:\API&Pipeline\archived\{}".format(file_name)
         shutil.copyfile(src_path, target_path)
         os.remove(src_path)
+        return True
