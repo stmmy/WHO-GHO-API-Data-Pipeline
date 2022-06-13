f011e2b15c1af8868d9b648f741910d57535c8f9
Matt Stamm <102386284+stmmy@users.noreply.github.com>
2022-06-13 15:30:28 -0500

    Update pipeline_queue.py

--- pipeline_queue.py
+++ pipeline_queue.py
@@ -1,16 +1,19 @@
 import pyodbc
-import os
 import glob
 import pandas as pd
 from datetime import datetime
 
 class pipeline_queue:
+    #Connection to the database
     def __init__(self):
         self.conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=BIGBERTHA;'
                       'Database=stayfit_data_metrics;'
                       'Trusted_Connection=yes;')
         self.cursor = self.conn.cursor()
+    
+    ###################
+    ### QUEUE FUNCTIONS
 
     #Returns a dataframe of files from selected step
     def GetStageFiles(self, StepToRetrieve):
@@ -43,14 +46,12 @@ class pipeline_queue:
         df = pd.read_sql(query, self.conn)
         i = 0
         for row in df.itertuples():
-            if row.File_Status == "DONE":
+            if row.File_Status == "DONE" or row.File_Status == "FAILED":
                 i += 1
-        
         if len(df) == i:
             return True
         else:
             return False
-        
 
     #Gets next file to feed through pipeline
     def GetNextFile(self):
@@ -92,3 +93,61 @@ class pipeline_queue:
     def ClearQueue(self):
         self.cursor.execute('''DELETE FROM pipeline_queue;''')
         self.conn.commit()
+    
+    #################
+    ### LOG FUNCTIONS
+
+    def GetLogs(self):
+        try:   
+            query = "SELECT [File_Name], [Log_Message], [Stage_Of_File] FROM log_data;"
+            df = pd.read_sql(query, self.conn)
+            return df
+        except Exception as exception_message:
+            print("Log Get Error:", exception_message)
+
+    #Updates the SQL log table with current log file logs
+    def UpdateLogs(self):
+        file_path = r"C:\API&Pipeline\logs.csv"
+        df = pd.read_csv(file_path, parse_dates=["Time_Stamp"], infer_datetime_format=True)
+        try:
+            for row in df.itertuples():
+                self.cursor.execute('''
+                        INSERT INTO log_data (File_Name, Log_Message, Time_Stamp, Stage_Of_File)
+                        VALUES (?,?,?,?)
+                        ''',
+                        row.File_Name,
+                        row.Log_Message,
+                        row.Time_Stamp,
+                        row.Stage_Of_File,
+                        )
+                self.conn.commit()
+        except Exception as exception_message:
+            print("Log Update Error:", exception_message)
+
+    #Clears SQL log table and resets local log file
+    def ClearLogs(self):
+        self.cursor.execute('''DELETE FROM log_data;''')
+        self.conn.commit()
+        with open("logs.csv", 'r+') as f:
+            f.readline()
+            f.truncate(f.tell())
+    
+    #Clears local logs, not sql table
+    def ClearLocalLogs(self):
+        with open("logs.csv", 'r+') as f:
+            f.readline()
+            f.truncate(f.tell())
+
+    #####################
+    ### CIGDATA FUNCTIONS
+    def ClearCigData(self):
+        self.cursor.execute("""DELETE FROM cigarette_metrics;""")
+        self.conn.commit()
+    
+    def GetCigData(self):
+        try:   
+            query = "SELECT [country_name], [country_code], [year], [sex], [value] FROM cigarette_metrics;"
+            df = pd.read_sql(query, self.conn)
+            return df
+        except Exception as exception_message:
+            print("Log Get Error:", exception_message)
