ed60c75c257285c0a00b17cd9086847fed4371cc
Matt Stamm <102386284+stmmy@users.noreply.github.com>
2022-06-23 14:59:52 -0500

    Update pipeline_controller.py

--- pipeline_controller.py
+++ pipeline_controller.py
@@ -3,22 +3,27 @@ import warnings
 import validate
 import process
 import transform
+import test_data
 import pipeline_queue as pq
 
 
 #Initialize and create basis for queue
 warnings.filterwarnings("ignore") #Constant warnings are thrown b/c of deprecated pandas functions
 queue = pq.pipeline_queue()
+queue.ClearLocalLogs()
+queue.ClearQueue()
+queue.ClearLogs()
 
-#Main Queue controller loop
 if len(os.listdir("C:/API&Pipeline/staging")) > 0:
     df = queue.GetStageFiles("staging")
     queue.AddToQueue(df)
     
+    #Main Queue controller loop
     while queue.CheckIfDone() != True:
         next_file = queue.GetNextFile()
 
         #Validation
+        print("Validating", next_file)
         queue.UpdateQueue(next_file, "VALIDATING")
         validate_file = validate.validate(next_file)
         if validate_file == False:
@@ -28,6 +33,7 @@ if len(os.listdir("C:/API&Pipeline/staging")) > 0:
             continue
 
         #Transformation
+        print("Transforming", next_file)
         queue.UpdateQueue(next_file, "TRANSFORMING")
         transform_file = transform.transform(next_file)
         if transform_file == False:
@@ -37,6 +43,7 @@ if len(os.listdir("C:/API&Pipeline/staging")) > 0:
             continue
 
         #Processing
+        print("Processing", next_file)
         queue.UpdateQueue(next_file, "PROCESSING")
         updated_file_name = next_file[0:next_file.find(".")] + ".csv"
         process_file = process.process(updated_file_name)
@@ -45,8 +52,13 @@ if len(os.listdir("C:/API&Pipeline/staging")) > 0:
         else:
             queue.UpdateQueue(next_file, "FAILED")
         queue.UpdateLogs()
+        
+        #Test data
+        print("Testing Data", next_file)
+        test_name = "control_file" + next_file[next_file.find("_"):]
+        test_data.test_data(test_name, next_file[:next_file.find(".")])
+        queue.UpdateLogs()
         queue.ClearLocalLogs()
-
     print("Queue Finished")
 else:
     print("No Files in staging")
